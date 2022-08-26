b487d8e17f80e35b8510099d47665e7322308872
Syed Mohammed Shazeb <75221729+syedmdshazeb986@users.noreply.github.com>
2022-08-26 17:57:03 +0530

    Update UserController.cs
    added booking action method

--- source/repos/Airlines_API/Airlines_API/Controllers/UserController.cs
+++ source/repos/Airlines_API/Airlines_API/Controllers/UserController.cs
@@ -123,37 +123,6 @@ namespace Airlines_API.Controllers
         }
 
 
-      [HttpPut("{id}")]
-        public ActionResult Put(int id, UserDetails modifieduser)
-        {
-            var data = _context.UserDetails.FirstOrDefault(u =>u.UserId  == id);
-            if (data == null)
-                return BadRequest();
-            else
-            {
-                data.Firstname = modifieduser.Firstname;
-                data.Lastname = modifieduser.Lastname;
-                data.Title = modifieduser.Title;
-                data.DateofBirth = modifieduser.DateofBirth;
-                data.Email = modifieduser.Email;
-                data.Password = modifieduser.Password;
-                data.Mobile = modifieduser.Mobile;
-                _context.SaveChanges();
-
-                return Ok();
-            }
-
-        }
-
-        [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
-        {
-            var temp = _context.UserDetails.FirstOrDefault(u => u.UserId == id);
-            _context.UserDetails.Remove(temp);
-            _context.SaveChanges();
-            return Ok();
-        }
-        
         [HttpGet]
         [Route("airports")]
         public ActionResult GetAirports()
@@ -174,6 +143,103 @@ namespace Airlines_API.Controllers
                     return Ok(result);
                 }
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+        
+        //book flight
+        [HttpPost]
+        [Route("booking")]
+        public ActionResult BookFlight([FromBody] BookingQuery query)
+        {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+
+                    return BadRequest(ModelState);
+
+                }
+
+                Flight flight = _context.Flights.FirstOrDefault(f => f.FlightId == query.FlightId);
+                decimal amount;
+
+
+                if (flight == null)
+                {
+                    return BadRequest("Flight does not exist");
+                }
+
+
+
+                if (query.Class_Type == "business")
+                {
+                    amount = flight.Business_fare;
+                }
+                else if (query.Class_Type == "economic")
+                {
+                    amount = flight.Economy_fare;
+                }
+                else
+                {
+                    return BadRequest("Invalid Flight Class Type");
+                }
+
+
+
+                if (query.Booking_Type == "one_way" || query.Booking_Type == "return") { }
+                else
+                {
+                    return BadRequest("Invalid booking type");
+                }
+                UserDetails u = _context.UserDetails.FirstOrDefault(user => user.UserId == query.UserId);
+                if (u == null)
+                {
+                    return BadRequest("Invalid User");
+
+                }
+
+                if (query.Payment_mode == "credit_card" || query.Payment_mode == "debit_card") { }
+                else
+                {
+                    return BadRequest("Invalid Payment mode");
+                }
+                List<BookingData> result = _context.GettingBookingData.FromSqlInterpolated($"exec dbo.SP_Book_Flight {query.UserId},{query.FlightId}, {query.Booking_Type}, {query.Return_Date}, {query.Passengers.FindAll(p => p.Age > 2).Count * amount},{DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss")}, {query.Class_Type}, {flight.Departure_time}, {query.Payment_mode}, {query.Passengers.Count}").ToList();
+
+                if (result != null && result.Count > 0)
+                {
+                    List<Seat> seats = _context.GetSeatsByFId.FromSqlInterpolated($"exec dbo.SP_Get_Seats_By_FlightId {flight.FlightId}").ToList();
+
+                    for (int i = 0; i < query.Passengers.Count; i++)
+                    {
+                        //check seat availibility
+                        Seat s = seats.FirstOrDefault(seat => seat.SeatId == query.Passengers[i].SeatId);
+                        if (s == null)
+                        {
+                            return BadRequest("Invalid Seat No.");
+                        }
+                        if (s.is_booked == true)
+                        {
+                            return BadRequest($"Seat {s.SeatName} ({s.SeatId}) is already booked");
+
+                        }
+                        var passenger = _context.Database.ExecuteSqlInterpolated($"exec dbo.SP_Add_Passengers {query.Passengers[i].Firstname},{query.Passengers[i].Lastname},{query.Passengers[i].Email}, {result[0].BookingId}, {query.Passengers[i].Phone_Number}, {query.Passengers[i].Age}, {query.Passengers[i].Gender},{query.Passengers[i].SeatId}");
+                        if (passenger == 0)
+                        {
+                            return StatusCode(StatusCodes.Status500InternalServerError, "Failed to add passengers");
+
+                        }
+                    }
+
+
+                    return Ok("Flight Booked Successfully");
+
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to book");
             }
             catch (Exception e)
             {
