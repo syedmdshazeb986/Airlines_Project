7ed98779fcfd3a3559793d444bb9fc86e5e2c99a
Syed Mohammed Shazeb <75221729+syedmdshazeb986@users.noreply.github.com>
2022-08-26 17:51:43 +0530

    Add files via upload

---
+++ source/repos/Airlines_API/Airlines_API/BookingData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airlines_API.Models
+{
+
+    public class BookingData
+    {
+        [Key]
+        [Required]
+        public long BookingId { get; set; }
+
+        [Required]
+        public long FlightId { get; set; }
+    }
+}
