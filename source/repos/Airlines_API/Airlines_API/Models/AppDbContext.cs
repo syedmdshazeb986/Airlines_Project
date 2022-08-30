1248c5a82cc95296831dd6af0d7ad08dcb23a943
Syed Mohammed Shazeb <75221729+syedmdshazeb986@users.noreply.github.com>
2022-08-30 12:32:24 +0530

    Update AppDbContext.cs

--- source/repos/Airlines_API/Airlines_API/Models/AppDbContext.cs
+++ source/repos/Airlines_API/Airlines_API/Models/AppDbContext.cs
@@ -15,15 +15,19 @@ namespace Airlines_API.Models
         public DbSet<UserDetails> UserDetails { get; set; }
 
         public DbSet<Admin> Admins { get; set; }
-        
+
         public DbSet<FlightModel> Flights { get; set; }
 
         public DbSet<Flight> AddFlights { get; set; }
 
         public DbSet<Airport> Airports { get; set; }
-        
-         public DbSet<SearchData> FilteredFlights { get; set; }
-         
+
         public DbSet<Seat> GetSeatsByFId { get; set; }
+
+        public DbSet<BookingData> GettingBookingData { get; set; }
+        public DbSet<GetBookings> GetBookings { get; set; }
+        public DbSet<Price> price { get; set; }
+        public DbSet<SearchData> FilteredFlights { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
     }
 }
