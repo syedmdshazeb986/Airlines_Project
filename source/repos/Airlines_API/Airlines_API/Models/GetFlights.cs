c2db838554fa9fef2076319f1b99efcaf625e7fd
Syed Mohammed Shazeb <75221729+syedmdshazeb986@users.noreply.github.com>
2022-08-30 13:17:56 +0530

    Add files via upload

---
+++ source/repos/Airlines_API/Airlines_API/Models/GetFlights.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airlines_API.Models
+{
+    public class GetFlights
+    {
+        [Key]
+        [Required]
+        public long FlightId { get; set; }
+
+        [Required]
+        public string FlightName { get; set; }
+
+
+        [Required]
+        public long Depart_airport_Id{ get; set; }
+
+        [Required]
+        public long Arrival_airport_Id { get; set; }
+
+
+        [Required]
+        public Nullable<DateTime> Departure_Time { get; set; }
+
+        [Required]
+        public Nullable<DateTime> Arrival_Time { get; set; }
+
+        [Required]
+        public decimal Economy_fare { get; set; }
+
+
+        [Required]
+        public decimal Business_fare { get; set; }
+
+        [Required]
+        public int Total_Business_Seats { get; set; }
+
+
+        [Required]
+        public int Total_Economy_Seats { get; set; }
+
+
+
+
+    }
+}
